ddce5c62b05cf41bbc84eddc90b5cef667022245
Qiuner <wdst3635@163.com>
2024-06-17 15:43:04 +0800

    P53 日志输出设置

---
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -0,0 +1,11 @@
+package com.hmall.api.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+
+public class DefaultFeignConfig {
+    @Bean
+    public Logger.Level feignLogLevel(){
+        return Logger.Level.FULL;
+    }
+}
