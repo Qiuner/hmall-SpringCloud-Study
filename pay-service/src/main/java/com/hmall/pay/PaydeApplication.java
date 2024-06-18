ce748fe64f0523c4666cb199a5e320d68f87c606
Qiuner <wdst3635@163.com>
2024-06-18 08:57:54 +0800

    支付服务完整拆分

---
+++ pay-service/src/main/java/com/hmall/pay/PaydeApplication.java
@@ -0,0 +1,17 @@
+package com.hmall.pay;
+
+
+import com.hmall.api.config.DefaultFeignConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(basePackages = "com.hmall.api.client",defaultConfiguration = DefaultFeignConfig.class)
+@MapperScan("com.hmall.pay.mapper")
+@SpringBootApplication
+public class PaydeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaydeApplication.class, args);
+    }
+}
