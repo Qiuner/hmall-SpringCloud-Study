856621596fa17db1b683bb09374e77d29a193e36
Qiuner <wdst3635@163.com>
2024-07-17 16:03:55 +0800

    feat:购物车商品服务拆分

---
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -0,0 +1,13 @@
+package com.hmall.cart;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.hmall.cart.mapper")
+@SpringBootApplication
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+}
