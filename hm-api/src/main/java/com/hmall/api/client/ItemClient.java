2bdd51a5623ae42e145b788079507156eb789321
Qiuner <wdst3635@163.com>
2024-07-20 09:47:59 +0800

    feat:拆分交易服务

--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -2,8 +2,11 @@ package com.hmall.api.client;
 
 
 import com.hmall.api.dto.ItemDTO;
+import com.hmall.api.dto.OrderDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
@@ -14,4 +17,8 @@ public interface ItemClient {
 
     @GetMapping("/items")
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+
+    @PutMapping("/items/stock/deduct")
+    void deductStock(@RequestBody List<OrderDetailDTO> items);
+
 }
