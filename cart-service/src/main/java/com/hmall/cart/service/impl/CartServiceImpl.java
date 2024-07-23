6b93960e06aeeb1121d01c530493d2c691d18681
Qiuner <wdst3635@163.com>
2024-07-23 20:52:54 +0800

    feat:nacos配置热更新

--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmall.api.client.ItemClient;
 import com.hmall.api.dto.ItemDTO;
+import com.hmall.cart.config.CartProperties;
 import com.hmall.cart.domain.dto.CartFormDTO;
 import com.hmall.cart.domain.po.Cart;
 import com.hmall.cart.domain.vo.CartVO;
@@ -41,6 +42,8 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
     // private final DiscoveryClient discoveryClient;
 
     private  final ItemClient itemClient;
+    private final CartProperties cartProperties;
+
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -165,8 +168,9 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
 
     private void checkCartsFull(Long userId) {
         int count = lambdaQuery().eq(Cart::getUserId, userId).count();
-        if (count >= 10) {
-            throw new BizIllegalException(StrUtil.format("用户购物车课程不能超过{}", 10));
+        if (count >= cartProperties.getMaxItems() ) {
+            throw new BizIllegalException(
+                     StrUtil.format("用户购物车课程不能超过{}", cartProperties.getMaxItems()));
         }
     }
 
