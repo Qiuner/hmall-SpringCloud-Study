ab609cb0f78ec36242250198c9cf59c9781079d0
Qiuner <wdst3635@163.com>
2024-07-26 21:55:22 +0800

    feat:fallback的使用

--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -1,6 +1,7 @@
 package com.hmall.api.client;
 
 
+import com.hmall.api.client.fallback.ItemClientFallback;
 import com.hmall.api.dto.ItemDTO;
 import com.hmall.api.dto.OrderDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Collection;
 import java.util.List;
 
-@FeignClient("item-service")
+@FeignClient(value = "item-service",fallbackFactory = ItemClientFallback.class)
 public interface ItemClient {
 
     @GetMapping("/items")
--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,5 +1,6 @@
 package com.hmall.api.config;
 
+import com.hmall.api.client.fallback.ItemClientFallback;
 import com.hmall.common.utils.UserContext;
 import feign.Logger;
 import feign.RequestInterceptor;
@@ -31,5 +32,10 @@ public class DefaultFeignConfig {
                 template.header("user-info", userId.toString());
             }
         };
+    }
+
+    @Bean
+    public ItemClientFallback itemClientFallback() {
+        return new ItemClientFallback();
     }
 }
